b5e9bc4123f5d5ac29a0d9c61ab971348a6ef883
teemal <taymal1987@gmail.com>
2020-02-15 19:20:46 -0800

    finished getting objects and sending to renderer

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var AWS = require("aws-sdk");
 
 const app = express();
 const port = 3000;
 app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
+const s3 = new AWS.S3({
+    region: 'us-east-1',
+    Bucket: 'thicc-boi-thiccbucket-1lqm0m2iu7gji'
+});
 
+let params = {
+    Bucket: "thicc-boi-thiccbucket-1lqm0m2iu7gji",
+};
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 });
 
+function assign(obj, keyPath, value) {
+    lastKeyIndex = keyPath.length - 1;
+    for (var i = 0; i < lastKeyIndex; ++i) {
+        key = keyPath[i];
+        if (!(key in obj)) {
+            obj[key] = {}
+        }
+        obj = obj[key];
+    }
+    obj[keyPath[lastKeyIndex]] = value;
+}
+var obj = {};
 app.get('/entities', (req, res) => {
-    res.send('this a test');
-
+    s3.listObjectsV2(params, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            data.Contents.forEach((i) => {
+                var res = i.Key.split("/");
+                assign(obj, res, i.Key)
+            })
+        }
+        res.send(obj);
+    })
 });
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
